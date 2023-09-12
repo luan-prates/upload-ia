c56ea52315862a84ecdffdcbf90fb41400535469
Luan Prates <pratesr21@gmail.com>
2023-09-12 17:16:41 +0000

    inicia o layout da aplicação

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # upload-ia
+
+Porjeto criado com vite 
+
+```pnpm create vite```
+
+instalar o [shadcn](https://ui.shadcn.com/docs/installation/vite)
--- upload-ia-web/src/App.tsx
+++ upload-ia-web/src/App.tsx
@@ -1,8 +1,25 @@
+import { Github } from 'lucide-react';
 import { Button } from "./components/ui/button";
 
 export function App() {
   return (
-    <Button>Teste</Button>
+    <div>
+      <div className="px-6 py-3 flex items-center justify-between border-b">
+        <h1 className="text-xl font-bold">upload.ia</h1>
+        <div className="flex items-center gap-3">
+          <span className="text-sm text-muted-foreground">
+            Desenvolvido com ❤️ por Luan Prates
+          </span>
+
+          <Separator></Separator>
+
+          <Button variant="outline">
+            <Github className="w-4 h-4 mr-2"/>
+            GitHub
+          </Button>
+        </div>
+      </div>
+    </div>
   )
 }
 
