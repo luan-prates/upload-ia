5b9d7224dd01e16201378fc9276473fcd1b3a421
Luan Prates <pratesr21@gmail.com>
2023-09-13 18:59:32 +0000

    cria `rota` `upload-videos.ts`

---
+++ upload-ia-server/src/routes/upload-video.ts
@@ -0,0 +1,9 @@
+import { FastifyInstance } from 'fastify'
+import { prisma } from '../lib/prisma'
+
+export async function uploadVideoRoute(app: FastifyInstance) {
+  app.post('/videos', async () => {
+    const prompts = await prisma.prompt.findMany()
+    return prompts
+  })
+}
